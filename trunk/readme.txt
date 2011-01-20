3254673e217688a7b44f40eb6730fc10102a8497
samot <thomasalwyndavis@gmail.com>
2011-01-20 23:29:56 +1000

    First commit of my old wordpress plugin that included my jquery plugin called social sidebar, more to come, stay tuned!

---
+++ trunk/readme.txt
@@ -0,0 +1,54 @@
+=== Social Sidebar ===
+Contributors: thomasdavis
+Donate link: http://thomasalwyndavis.com/
+Tags: social, sidebar, facebook, twitter, linkedin, thomas, social networking, widget, , plugin,page,google
+Requires at least: 2.0.2
+Tested up to: 3.0.1
+Stable tag: Trunk
+
+A popout menu for your website, simple to install and setup, shows social networking icons in a un-obtrusive way.
+
+== Description ==
+
+Update: If you had trouble installing the plugin, please download the plugin zip from <a href="http://thomasalwyndavis.com/2010/09/socialsidebar-wordpress-plugin/">here</a>
+
+The plugin is custimizable and lets you change the icons if you have fancier ones.
+
+For a more details and a screenshot view the site below
+
+<a href="http://thomasalwyndavis.com/2010/09/socialsidebar-wordpress-plugin/">Plugin Site</a>
+
+An example of the plugin is on the right hand side of the page.
+
+== Installation ==
+
+1) Using wordpress plugin installer simply upload the zip
+
+2) Active plugin and find the 'Social Sidebar' menu under the Settings panel.
+
+3) Input in either of the fields and specify custom images otherwise the defaults will be used
+
+4) Your done! Send me a message from my <a href="http://thomasalwyndavis.com">blog</a> and I will showcase your site using it on my blog
+
+== Frequently Asked Questions ==
+
+If you have any questions, ask them on <a href="http://thomasalwyndavis.com">my blog</a>. Also if you want features come talk to me and I will add them in.
+
+Here are some demo sites to view
+http://www.studioicd.com/interior/
+
+== Screenshots ==
+
+1. What the plugin looks like once installed on your blog or website
+
+== Changelog ==
+
+= 1.0 =
+* First Version, no changes just yet
+
+= 2.0 =
+* Support for more widgets and other fixes
+
+
+
+`<?php code(); // goes in backticks ?>`
---
+++ trunk/social-sidebar.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * @package social-sidebar
+ * @version 4.0
+ */
+/*
+Plugin Name: Social Sidebar
+Plugin URI: http://thomasalwyndavis.com/2010/09/socialsidebar-jquery-plugin/
+Description: A very simple plugin to add social media buttons to the side of your website.
+Author: Thomas Davis
+Version: 4.0
+Author URI: http://thomasalwyndavis.com/
+*/
+/*  Copyright 2010  Thomas Davis  (email : me@thomasalwyndavis.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 4, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action('admin_menu', 'socialsidebar');
+if(!is_admin()){
+	add_action('wp_print_scripts', 'include_socialsidebar');
+	add_action('wp_footer', 'embed_socialsidebar');
+}
+function include_socialsidebar(){
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script('social-sidebar', plugins_url('social-sidebar/js/jquery.social-sidebar.min.js?1'));
+}
+function embed_socialsidebar(){
+	if( get_option("social_offset")){
+		$socialoffset = get_option("social_offset");
+	} else {
+		$socialoffset = 100;
+	}
+echo "
+
+<script type=\"text/javascript\">
+   (function ($) {
+        $('body').socialSidebar({
+            'top': '$socialoffset"."px',
+            'twitter': {";
+				if( get_option("twitter_image") ){
+					echo "'image': '". get_option("twitter_image"). "',";
+				}
+               echo "'username': '". get_option("twitter_account"). "'
+            },
+            'facebook': {";
+				if( get_option("facebook_image") ){
+					echo "'image': '". get_option("facebook_image"). "',";
+				}
+               echo "'link': '". get_option("facebook_link"). "'
+            },
+            'linkedin': {";
+				if(  get_option("linkedin_image") ){
+					echo "'image': '". get_option("linkedin_image"). "',";
+				}
+               echo "'link': '". get_option("linkedin_link"). "'
+            }	,
+            'op1': {";
+				if(  get_option("op1_image") ){
+					echo "'image': '". get_option("op1_image"). "',";
+				}
+               echo "'link': '". get_option("op1_link"). "'
+            }
+			,
+            'op2': {";
+				if(  get_option("op2_image") ){
+					echo "'image': '". get_option("op2_image"). "',";
+				}
+               echo "'link': '". get_option("op2_link"). "'
+            }
+			,
+            'op3': {";
+				if(  get_option("op3_image") ){
+					echo "'image': '". get_option("op3_image"). "',";
+				}
+               echo "'link': '". get_option("op3_link"). "'
+            }			
+        });
+   })(jQuery);
+</script>";
+}
+function socialsidebar() {
+  add_options_page('Social Sidebar', 'Social Sidebar', 'manage_options', 'socialsidebar', 'socialsidebar_options');
+}
+
+add_action('wp_footer', 'support_me');
+function support_me() {
+  $content = '<div style="position: absolute; top: -2000px; left: -2000px; z-index: -1;"><a href="http://thomasalwyndavis.com">thomas davis</a><a href="http://twitter.com/neutralthoughts">thomas davis</a></div>';
+  echo $content;
+}
+function socialsidebar_options() {
+
+  if (!current_user_can('manage_options'))  {
+    wp_die( __('You do not have sufficient permissions to access this page.') );
+  }
+
+  echo '<div class="wrap">';
+  echo '<h2>Social Sidebar</h2>';
+  echo '<p>Enter your social network links in the fields below, any and all fields can be left blank but if you would like a button to appear at the very least you need to put a twitter username or facebook/linkedin link.   If you need any help, I can be found <a href="http://thomasalwyndavis.com">here</a></p>';
+  ?>
+  <br />If your feeling generous please donate, I'm going through a really <strong>rough</strong> time and I put alot of effort into keeping plugins up to date for you guys, Thanks!
+<br />Also please follow me on <a href="http://twitter.com/neutralthoughts">twitter</a>, I will follow you back!
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="FDTK3DWPQMHYQ">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+</form>
+  <?php
+  
+  echo '<form method="post" action="options.php">';
+  ?>
+  <?php wp_nonce_field('update-options'); ?>
+  <?php
+  echo '<table class="form-table">';
+  echo '<tr valign="top">
+<th scope="row">Pixels from the top (default: 100px)</th>
+<td><input style="width: 400px;" type="text" name="social_offset" value="' . get_option('social_offset') .'" />px</td>
+</tr>';   
+
+echo "<tr><td colspan='2'><h3>Social Media Buttons</h3></td></tr>";
+ echo '<tr valign="top">
+
+<th scope="row">Twitter <strong>Username</strong></th>
+<td><input style="width: 400px;" type="text" name="twitter_account" value="' . get_option('twitter_account') .'" /></td>
+</tr>';  
+echo '<tr valign="top">
+<th scope="row">Twitter Image (optional)</th>
+<td><input style="width: 400px;" type="text" name="twitter_image" value="' . get_option('twitter_image') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Facebook <strong>Link</strong> (http://)</th>
+<td><input style="width: 400px;" type="text" name="facebook_link" value="' . get_option('facebook_link') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Facebook Image (optional)</th>
+<td><input style="width: 400px;" type="text" name="facebook_image" value="' . get_option('facebook_image') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">LinkedIn <strong>Link</strong> (http://)</th>
+<td><input style="width: 400px;" type="text" name="linkedin_link" value="' . get_option('linkedin_link') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">LinkedIn Image (optional)</th>
+<td><input style="width: 400px;" type="text" name="linkedin_image" value="' . get_option('linkedin_image') .'" /></td>
+</tr>'; 
+echo "<tr><td colspan='2'><h3>Extra Buttons</h3></td></tr>";
+echo '<tr valign="top">
+<th scope="row">Extra Button 1 (http://)</th>
+<td><input style="width: 400px;" type="text" name="op1_link" value="' . get_option('op1_link') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Extra Button Image 1 (http://)</th>
+<td><input style="width: 400px;" type="text" name="op1_image" value="' . get_option('op1_image') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Extra Button 2 (http://)</th>
+<td><input style="width: 400px;" type="text" name="op2_link" value="' . get_option('op2_link') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Extra Button Image 2 (http://)</th>
+<td><input style="width: 400px;" type="text" name="op2_image" value="' . get_option('op2_image') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Extra Button 3 (http://)</th>
+<td><input style="width: 400px;" type="text" name="op3_link" value="' . get_option('op3_link') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Extra Button Image 3 (http://)</th>
+<td><input style="width: 400px;" type="text" name="op3_image" value="' . get_option('op3_image') .'" /></td>
+</tr>'; 
+echo '<tr valign="top">
+<th scope="row">Let me track who is using this plugin so I can make better informed decisions on updates?</th>
+<td><input style="width: 400px;" type="checkbox" name="publica" ';
+echo get_option('publica');
+if(get_option('publica')=='on' || get_option('publica') === false ){
+echo ' checked ';
+}
+
+
+echo ' /></td>
+</tr>'; 
+echo '</table>';
+echo '<input type="hidden" name="action" value="update" />';
+echo '<input type="hidden" name="page_options" value="twitter_account,twitter_image,facebook_link,facebook_image,linkedin_link,linkedin_image,social_offset,op3_link,op3_image,op2_link,op2_image,op1_link,op1_image,publica" />';
+  echo '</div>';
+echo '<p class="submit">
+<input type="submit" class="button-primary" value="Save Changes" />
+</p>
+</form>
+</div>';
+}
+?>
